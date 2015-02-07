1b702008fc29b7ab517c6e5358e7bb9d8627cf91
Bharadwaj <bharadwaj.ramachandran@gmail.com>
2015-02-06 22:18:49 -0500

    updated titleParse.py

--- titleParse.py
+++ titleParse.py
@@ -3,24 +3,23 @@ import sys
 import urllib2
 
 def URLtoTitle(url):
-    assert(url.startswith("http"))
-    try:
-        response = urllib2.urlopen(url)
-        page_source = response.read()
-        soup = BeautifulSoup(page_source)
-    finally:
-        if (response != None): response.close()
-    if (soup.title == None): return ''
-    else: return soup.title.string
-
-def readWebPage(url):
-    fin = contents = None
-    try:
-        fin = urllib.urlopen(url)
-        contents = fin.read()
-    finally:
-        if (fin != None): fin.close()
-    return contents
+    if url == '':
+        return ''
+    else:
+        print url
+        response = soup = None
+        assert(url.startswith("http"))
+        try:
+            response = urllib2.urlopen(url)
+            page_source = response.read()
+            soup = BeautifulSoup(page_source)
+        finally:
+            if (response != None): 
+                response.close()
+        if (soup.title == None):
+            return ''
+        else:
+            return soup.title.string
 
 def readFile(filename, mode="rt"):
     # rt stands for "read text"
@@ -32,8 +31,22 @@ def readFile(filename, mode="rt"):
         if (fin != None): fin.close()
     return contents
 
+def removeSource(raw_text):
+    data_arr = raw_text.split()
+    len_d = len(data_arr)
+    if (len_d > 1):
+        print data_arr
+        return data_arr[1]
+    elif (len_d == 0):
+        return ''
+    else:
+        print data_arr
+        return ''
+
 def getTitles(textfile):
-    URL_list = (readFile(textfile)).split()
+    raw_file = readFile(textfile).split('\n')
+    last_elem = raw_file.pop()
+    URL_list = map(removeSource, raw_file)
     return map(URLtoTitle, URL_list)
 
 print getTitles(sys.argv[1])
