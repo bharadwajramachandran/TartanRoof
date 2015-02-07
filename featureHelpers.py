97aeb496d27bebb88232a6783ba0e51e933f0b27
Suhaas Reddy <suhaasr@andrew.cmu.edu>
2015-02-07 10:10:29 -0500

    Put helper functions together

--- featureHelpers.py
+++ featureHelpers.py
@@ -1,11 +1,11 @@
 import enchant, re
 
-def strip_symbols(string):
-	return re.sub('[1234567890!@#$%^&*()_+=[]{}|\;:,./?<>~]', '', string)
+def strip_symbols(s):
+	return re.sub('[1234567890!@#$%^&*()_+=[]{}|\;:,./?<>~]', '', s)
 
-def is_eng_word(string):
+def is_eng_word(s):
 	ENG_dict = enchant.Dict("en_US")
-	return ENG_dict.check(string)
+	return ENG_dict.check(s)
 
 def misspelling_ratio(CL_listing):
 	words = CL_listing.split()
@@ -14,3 +14,9 @@ def misspelling_ratio(CL_listing):
 	total_num_of_words = len(words)
 	return float(misspelled_count)/float(total_num_of_words)
 
+def countParagraphs(message):
+    count = 0
+    for c in message:
+        if c == '\n':
+            count += 1
+    return count
